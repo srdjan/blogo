38980a4e063be041ec4029c354dbddf2028958cf
srdjan <srdjan@timok.com>
2025-06-15 12:04:48 -0400

    more about page edits

--- src/components/AboutHtml.tsx
+++ src/components/AboutHtml.tsx
@@ -2,22 +2,16 @@ export function AboutHtml() {
   return (
     <section>
       <article>
+        <h3>Welcome to my corner of the internet.</h3>
         <p>
-          Welcome to my corner of the internet. Here is where I write about my musings and opinions
+          Here is where I write about my musings and opinions
           about all product & technology topics that interest me. 
           
-          And who am I? My name is Srdjan and I'm a {" "}<strong>NYC-area technologist</strong>{" "}
+          I'm a {" "}<strong>NYC-area technologist</strong>{" "} 
           with a passion for turning big ideas into resilient, impactful products.
-
-          My background as a recovering startup founder and ex-<strong> ThoughtWorks principal
+          My background as a recovering startup founder and ex-<strong> ThoughtWorks Technical Principal
           </strong>{" "} means I speak both the language of engineers and executives.
         </p>
-
-        <aside>
-          <p>
-            My proudest moments? The ones that materialize as{" "} <strong>team success</strong>.
-          </p>
-        </aside>
 
         <hr />
 
