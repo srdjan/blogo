3e162743c3770ccf59928f871204b1826173c01a
srdjan <srdjan@timok.com>
2025-06-15 12:04:26 -0400

    updated About page

--- src/components/AboutHtml.tsx
+++ src/components/AboutHtml.tsx
@@ -3,48 +3,21 @@ export function AboutHtml() {
     <section>
       <article>
         <p>
-          Welcome to my corner of the internet. I'm a{" "}
-          <strong>NYC-area technologist</strong>{" "}
-          with a passion for turning big ideas into resilient, impactful
-          products.
-        </p>
+          Welcome to my corner of the internet. Here is where I write about my musings and opinions
+          about all product & technology topics that interest me. 
+          
+          And who am I? My name is Srdjan and I'm a {" "}<strong>NYC-area technologist</strong>{" "}
+          with a passion for turning big ideas into resilient, impactful products.
 
-        <p>
-          Complex tech doesn't need to be a mystery. Whether it's architecting
-          {" "}
-          <strong>modern solutions</strong>, designing{" "}
-          <strong>NFT ticketing systems</strong>, or rethinking{" "}
-          <strong>decentralized hiring</strong>, I translate chaos into clear
-          roadmaps.
-        </p>
-
-        <p>
-          My background as a recovering startup founder and ex-<strong>
-            ThoughtWorks principal
-          </strong>{" "}
-          means I speak both the language of engineers and executives.
+          My background as a recovering startup founder and ex-<strong> ThoughtWorks principal
+          </strong>{" "} means I speak both the language of engineers and executives.
         </p>
 
         <aside>
           <p>
-            My proudest moments? The ones that materialize as{" "}
-            <strong>team success</strong>.
+            My proudest moments? The ones that materialize as{" "} <strong>team success</strong>.
           </p>
         </aside>
-
-        <p>
-          This space is where I unpack the{" "}
-          <strong>intersections of tech, culture, and creativity</strong>. I'll
-          share lessons from my{" "}
-          <strong>20+ years in tech</strong>, but also the stories behind the
-          code.
-        </p>
-
-        <p>
-          You'll find late-night debugging sessions, messy collaborations, and
-          the occasional typo that sparked a breakthrough. And,{" "}
-          <em>My friend, Claude, will help me not lose my train of thought.</em>
-        </p>
 
         <hr />
 
