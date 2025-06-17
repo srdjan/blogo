62db06763a80f67b369daf92d6a40ac9c31ce53a
srdjan <srdjan@timok.com>
2025-06-17 08:24:16 -0400

    edited about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,25 @@ export function About() {
   return (
     <section>
       <article>
-        <h3>Welcome to my corner of the internet.</h3>
         <p>
-          Here is where I write about my musings and opinions
-          about all product & technology topics that interest me. 
-          
-          I'm a {" "}<strong>NYC-area technologist</strong>{" "} 
+          Hi, I'm Srdjan,
+          <a
+            href="https://srdjan.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>⊣˚∆˚⊢</span>
+          </a>, a recovering startup founder and ex-<strong>ThoughtWorks Technical Principal</strong>{" "} 
           with a passion for turning big ideas into resilient, impactful products.
-          My background as a recovering startup founder and ex-<strong> ThoughtWorks Technical Principal
-          </strong>{" "} means I speak both the language of engineers and executives.
+        </p>
+
+        <h3>Welcome to my corner of the internet.</h3>
+
+        <p>
+        This blog captures my experiences, from navigating leadership challenges to exploring emerging technologies
+        like generative AI, blockchain, and cloud-native architectures. I'll write about my musings and opinions
+        on product, architecture, agile engineering, & other technology topics that interest me. 
+        I hope you find something of value here.
         </p>
 
         <hr />
