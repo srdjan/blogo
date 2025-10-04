ad89b5fd295fa25d3c7f8c9913b352274976f841
Srdjan <srdjan@timok.com>
2025-10-04 08:57:56 -0400

    font change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -71,7 +71,7 @@ export const createLayout = (props: LayoutProps): Response => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&display=swap"
           rel="stylesheet"
         />
         <link rel="manifest" href="/manifest.json" />
