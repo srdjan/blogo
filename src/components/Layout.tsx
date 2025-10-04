5c5cc1485a6916879faf802ecb73ac5371c23105
Srdjan <srdjan@timok.com>
2025-10-04 08:36:46 -0400

    font adjustments

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -70,6 +70,10 @@ export const createLayout = (props: LayoutProps): Response => {
         <link rel="dns-prefetch" href="//fonts.gstatic.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
+          rel="stylesheet"
+        />
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="alternate"
